9ca06518944524f326520ab282a0a55940fde5c9
ABDEV <abdellahyeoussoumass@gmail.com>
2026-05-08 00:00:15 +0100

    Add initial scripts.c with execute_script function
    Initial implementation of execute_script function with placeholders for future functionality.

---
+++ src/scripts.c
@@ -0,0 +1,12 @@
+#include "../include/scripts.h"
+#include "../include/parser.h"
+#include "../include/executor.h"
+#include <stdio.h>
+
+// TODO: P5 will implement this
+
+int execute_script(char *filename)
+{
+    // TODO: Read file, parse each line, execute
+    return 0;
+}
