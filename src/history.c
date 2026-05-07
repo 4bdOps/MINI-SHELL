d5921f52fb52239d69a1774971a2416509d140bd
ABDEV <abdellahyeoussoumass@gmail.com>
2026-05-07 23:58:49 +0100

    Implement history management functions
    Added functions for managing command history.

---
+++ src/history.c
@@ -0,0 +1,27 @@
+#include "../include/history.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+// TODO: P3 will implement these
+
+void add_to_history(char *command)
+{
+    // TODO: Store in memory
+}
+
+void print_history(void)
+{
+    // TODO: Display history
+}
+
+void save_history(void)
+{
+    // TODO: Save to ~/.mini_shell_history
+}
+
+void load_history(void)
+{
+    // TODO: Load from ~/.mini_shell_history
+}
