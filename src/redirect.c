869fb80ce6d3e3053bd36b5c263812062b26cd41
ABDEV <abdellahyeoussoumass@gmail.com>
2026-05-07 23:59:40 +0100

    Add redirection handling functions
    Implemented initial structure for redirection handling.

---
+++ src/redirect.c
@@ -0,0 +1,15 @@
+#include "../include/redirect.h"
+#include <stdio.h>
+
+// TODO: P4 will implement these
+
+int handle_redirection(t_command *cmd)
+{
+    // TODO: Open files, dup2
+    return 0;
+}
+
+void restore_redirection(void)
+{
+    // TODO: Restore stdin/stdout
+}
