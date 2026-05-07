79d0747e20683aba888bc8a349795eedb0b999da
ABDEV <abdellahyeoussoumass@gmail.com>
2026-05-08 00:40:37 +0100

    Update history.c

--- src/history.c
+++ src/history.c
@@ -1,4 +1,4 @@
-#include "../include/history.h"
+ذ#include "../include/history.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,22 +6,22 @@
 
 // TODO: P3 will implement these
 
-void add_to_history(char *command)
+void add_to_history(char *command __attribute__((unused)))
 {
-    // TODO: Store in memory
+	// TODO: Store in memory
 }
 
 void print_history(void)
 {
-    // TODO: Display history
+	// TODO: Display history
 }
 
 void save_history(void)
 {
-    // TODO: Save to ~/.mini_shell_history
+	// TODO: Save to ~/.mini_shell_history
 }
 
 void load_history(void)
 {
-    // TODO: Load from ~/.mini_shell_history
+	// TODO: Load from ~/.mini_shell_history
 }
